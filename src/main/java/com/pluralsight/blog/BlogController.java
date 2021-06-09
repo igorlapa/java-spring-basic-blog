3b612469584463f228e98d3e0b379a48a3fb935a
Igor <11443481+igorlapa@users.noreply.github.com>
2021-06-09 20:11:18 +0200

    Revert "Creating a request mapping method and add @RequestMapping Annotation to it"
    This reverts commit 9011a455386b8756ed9e0a992a6bb841a91931a8.

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,8 @@
 package com.pluralsight.blog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class BlogController {
-
-    @RequestMapping("/")
-    public String listPosts() {
-        return "home";
-    }
 
 }
